03d4768c01e1d0f580206b3972e4c1338de0f658
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2019-03-12 11:36:40 +0100

    Step1

---
+++ rest/src/main/java/com/example/webappdemo/AppConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.webappdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * This class is only used for testing and in production version it should be omitted.
+ *
+ * @author Vojtech Sassmann <vojtech.sassmann@gmail.com>
+ */
+@Configuration
+public class AppConfiguration extends WebMvcConfigurationSupport  {
+	@Override
+	protected void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
+				.allowedMethods(
+						"GET",
+						"POST",
+						"PUT",
+						"DELETE",
+						"OPTIONS",
+						"HEAD")
+				.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
+				.allowCredentials(true);
+	}
+}
